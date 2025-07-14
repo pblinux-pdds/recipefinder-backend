b6f2e9a23a0a1be1d13e7335af1a63b1b99d8a99
Pablo Bautista <pablo@pixela.tech>
2025-07-13 18:29:51 -0600

    add: fields and relations for Receipe

--- app/models.py
+++ app/models.py
@@ -1,10 +1,33 @@
 from . import db
 
+recipe_ingredients = db.Table(
+    'recipe_ingredients',
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
+    db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
+)
+
+recipe_categories = db.Table(
+    'recipe_categories',
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    instructions = db.Column(db.Text)
+    prep_time = db.Column(db.Integer)
+    cook_time = db.Column(db.Integer)
+    servings = db.Column(db.Integer)
+    image_url = db.Column(db.String(255))
+    ingredients = db.relationship('Ingredient', secondary=recipe_ingredients, backref='recipes')
+    categories = db.relationship('Category', secondary=recipe_categories, backref='recipes')
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
